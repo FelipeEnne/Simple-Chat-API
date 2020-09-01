ffd8f9626dd18bfdaffb153a6911dd3225ab4f7b
FelipeEnne <felipeenne@gmail.com>
2020-09-01 17:14:27 -0300

    add enter chat

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-unused-vars */
+// /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 // /* eslint-disable prefer-const */
-const socker = io();
+const socket = io();
 let userName = '';
-const userList = [];
+let userList = [];
 
 const loginPage = document.querySelector('#loginPage');
 const chatPage = document.querySelector('#chatPage');
@@ -14,12 +14,32 @@ const textInput = document.querySelector('#chatTextInput');
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
 
+function renderUserList() {
+  const ul = document.querySelector('.userList');
+  ul.innerHTML = '';
+
+  userList.forEach(i => {
+    ul.innerHTML += `<li>${i}</li>`;
+  });
+}
+
 loginInput.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
     const name = loginInput.value.trim();
     if (name !== '') {
       userName = name;
       document.title = `Chat (${userName})`;
+
+      socket.emit('join-request', userName);
     }
   }
+});
+
+socket.on('user-ok', (list) => {
+  loginPage.style.display = 'none';
+  chatPage.style.display = 'flex';
+  textInput.focus();
+
+  userList = list;
+  renderUserList();
 });
