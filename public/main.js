fb85b38b3e46c26a18049f0166c0d736bdc1476e
FelipeEnne <felipeenne@gmail.com>
2020-09-01 16:52:27 -0300

    add event listener

--- public/main.js
+++ public/main.js
@@ -1,3 +1,25 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable-next-line no-undef */
+/* eslint-disable no-undef */
+// /* eslint-disable prefer-const */
 const socker = io();
+let userName = '';
+const userList = [];
+
+const loginPage = document.querySelector('#loginPage');
+const chatPage = document.querySelector('#chatPage');
+
+const loginInput = document.querySelector('#loginNameInput');
+const textInput = document.querySelector('#chatTextInput');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+loginInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    const name = loginInput.value.trim();
+    if (name !== '') {
+      userName = name;
+      document.title = `Chat (${userName})`;
+    }
+  }
+});
